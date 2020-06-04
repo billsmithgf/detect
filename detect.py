762255f5baac23a2402600c3d5d4fbabfa1a3f46
billsmithgf <62889826+billsmithgf@users.noreply.github.com>
2020-06-04 22:59:57 +1000

    Add files via upload

---
+++ detect.py
@@ -0,0 +1,38 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+TRIG = 18
+ECHO = 24
+buzzer = 3
+led = 2
+print("Distance Measurement in progress")
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
+GPIO.setup(led, GPIO.OUT)
+GPIO.setup(buzzer, GPIO.OUT)
+
+while True:
+   GPIO.output(TRIG, False)
+   time.sleep(1)
+   GPIO.output(TRIG, True)
+   time.sleep(0.01)
+   GPIO.output(TRIG, False)
+   while GPIO.input(ECHO)==0:
+      pulse_start = time.time()
+
+   while GPIO.input(ECHO)==1:
+      pulse_end = time.time()
+
+   pulse_duration = pulse_end - pulse_start
+
+   distance = pulse_duration*11150
+   distance = round(distance, 2)
+
+   if distance < 60:
+      GPIO.output(buzzer, GPIO.HIGH)
+      GPIO.output(led, GPIO.LOW)
+      print("KEEP YOU DISTANCE")
+   else:
+      print("SAFE DISTANCE")
+      GPIO.output(buzzer, GPIO.LOW)
+      GPIO.output(led, GPIO.HIGH)
